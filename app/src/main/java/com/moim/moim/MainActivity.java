8c5647a8290e87c9471de42c44ded6f565fc2468
LeeSungHee <esack1985@naver.com>
2017-06-24 16:55:56 +0900

    test11112323

--- app/src/main/java/com/moim/moim/MainActivity.java
+++ app/src/main/java/com/moim/moim/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
-//sadasdsda
+//sadasdsdadsfs
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
