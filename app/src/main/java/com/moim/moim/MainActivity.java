29bb6c0e62b6821088a6e2f0b6b47b1a91f1a2df
iamkimpj <iamkimpj@gmail.com>
2017-06-25 17:56:06 +0900

    fragment test

--- app/src/main/java/com/moim/moim/MainActivity.java
+++ app/src/main/java/com/moim/moim/MainActivity.java
@@ -3,9 +3,17 @@ package com.moim.moim;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import com.moim.moim.meet.MeetFragment;
+
+import static com.moim.moim.R.id.fragment_meet;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,25 +21,25 @@ public class MainActivity extends AppCompatActivity {
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
-//sadasdsdadsssadasdsdasddafzxczxc
+
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText("벙개");
+                    mTextMessage.setText(R.string.title_meet);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText("모임");
+                    mTextMessage.setText(R.string.title_group);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText("설정");
+                    mTextMessage.setText(R.string.title_setting);
                     return true;
             }
             return false;
         }
 
     };
-
+//sd
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,6 +48,16 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        Fragment fragment = new MeetFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.fragment_meet, fragment);
+        fragmentTransaction.commit();
+
+
+
+
     }
 
 }
