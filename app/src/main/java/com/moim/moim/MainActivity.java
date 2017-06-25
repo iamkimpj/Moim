3df8079569b9a43996c54465b3e24c26cc3ae79a
iamkimpj <iamkimpj@gmail.com>
2017-06-25 16:50:02 +0900

    네비게이션 명칭 변경

--- app/src/main/java/com/moim/moim/MainActivity.java
+++ app/src/main/java/com/moim/moim/MainActivity.java
@@ -18,13 +18,13 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    mTextMessage.setText("벙개");
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    mTextMessage.setText("모임");
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    mTextMessage.setText("설정");
                     return true;
             }
             return false;
