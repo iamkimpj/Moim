71fe082e2b270c7a7ab35be4d399aad8db290023
iamkimpj <iamkimpj@gmail.com>
2017-06-29 18:50:45 +0900

    User모델과 Auth 컴프 세팅

---
+++ app/src/main/java/com/moim/moim/Comp/Auth.java
@@ -0,0 +1,13 @@
+package com.moim.moim.Comp;
+
+import com.moim.moim.Model.User;
+
+/**
+ * Created by iamkimpj on 2017-06-29.
+ */
+
+public class Auth {
+    public User user() {
+        return new User();
+    }
+}
---
+++ app/src/main/java/com/moim/moim/Model/User.java
@@ -0,0 +1,8 @@
+package com.moim.moim.Model;
+
+/**
+ * Created by iamkimpj on 2017-06-29.
+ */
+
+public class User {
+}
