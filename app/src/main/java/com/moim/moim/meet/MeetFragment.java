5d7518178f9581005ef787c199aa414ed551b567
younsu <zepping@naver.com>
2017-07-04 18:34:09 +0900

    adapter R이상

--- app/src/main/java/com/moim/moim/meet/MeetFragment.java
+++ app/src/main/java/com/moim/moim/meet/MeetFragment.java
@@ -121,6 +121,7 @@ public class MeetFragment extends Fragment {
      */
 //    public interface OnFragmentInteractionListener {
 //        // TODO: Update argument type and name
+    //
 //        void onFragmentInteraction(Uri uri);
 //    }
 }
