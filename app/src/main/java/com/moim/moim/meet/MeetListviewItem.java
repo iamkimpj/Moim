39d1c255377f7bf02c387e382ec3587085d20483
younsu <zepping@naver.com>
2017-07-04 20:30:41 +0900

    Adapter 오류

---
+++ app/src/main/java/com/moim/moim/meet/MeetListviewItem.java
@@ -0,0 +1,33 @@
+package com.moim.moim.meet;
+
+/**
+ * Created by ltcare on 2017-07-04.
+ */
+
+public class MeetListviewItem {
+    String dayofweek;
+    String today;
+    String totime;
+    String todaytime;
+    String place;
+    String price;
+    String pricetext;
+    String title;
+    int resId;
+    int resId1;
+
+    public MeetListviewItem(String dayofweek, String today, String totime, String todaytime, String place, String  price, String pricetext, String title, int resId, int resId1){
+        this.dayofweek = dayofweek;
+        this.today = today;
+        this.totime = totime;
+        this.todaytime = todaytime;
+        this.place = place;
+        this.price = price;
+        this.pricetext = pricetext;
+        this.title = title;
+        this.resId = resId;
+        this.resId1 = resId1;
+    }
+
+
+}
